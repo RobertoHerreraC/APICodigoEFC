4b87440e2fd38f204c8b11a7695b3b00c654d9b4
Roberto Herrera <robert_hc_95@hotmail.com>
2024-09-04 22:59:55 -0500

    Add project files.

---
+++ APICodigoEFC/Controllers/CustomersController.cs
@@ -0,0 +1,99 @@
+﻿using APICodigoEFC.Models;
+using APICodigoEFC.Request;
+using APICodigoEFC.Response;
+using APICodigoEFC.Utility;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICodigoEFC.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly CodigoContext _context;
+
+        public CustomersController(CodigoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public List<Customer> GetByFilters(string? name,string? documentNumber )
+        {
+            IQueryable<Customer> query = _context.Customers.Where(x=>x.IsActive);
+
+            if (!string.IsNullOrEmpty(name))
+                query = query.Where(x => x.Name.Contains(name));
+
+            if (!string.IsNullOrEmpty(documentNumber))
+                query = query.Where(x => x.DocumentNumber.Contains(documentNumber));
+
+            return query.ToList();
+        }
+
+        [HttpPost]
+        public void Insert([FromBody] Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+        }
+        [HttpPut]
+        public void Update([FromBody] Customer customer)
+        {
+            _context.Entry(customer).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        [HttpPut]
+        public ResponseBase UpdateName([FromBody] CustomerUpdateRequest request)
+        {
+            ResponseBase response = new ResponseBase();
+
+            try
+            {                
+
+                var customer = _context.Customers.Find(request.Id);
+                if (customer == null)
+                {
+                    response.Code = -1001;
+                    response.Message = Validations.ExistCustomer;
+                    return response;
+                }
+                customer.Name = request.Name;
+                _context.Entry(customer).State = EntityState.Modified;
+                _context.SaveChanges();
+
+                response.Message = Validations.CorrectUpdate;
+
+                return response;
+
+            }
+            catch (Exception ex)
+            {
+                //Write log
+                response.Message = Validations.GeneralError;
+                response.Code = -1000;
+                return response;
+            }
+         
+
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            //Eliminación Física  
+            //_context.Customers.Remove(customer);
+
+            var customer =  _context.Customers.Find(id);
+            customer.IsActive = false;
+            _context.Entry(customer).State = EntityState.Modified;         
+            _context.SaveChanges();          
+        }
+
+
+
+    }
+}
---
+++ APICodigoEFC/Controllers/ProductsController.cs
@@ -0,0 +1,77 @@
+﻿using APICodigoEFC.Models;
+using APICodigoEFC.Request;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICodigoEFC.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    //[Authorize]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly CodigoContext _context;
+
+        public ProductsController(CodigoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public List<Product> GetByFilters(string? name)
+        {
+            IQueryable<Product> query = _context.Products.Where(x => x.IsActive);
+
+            if (!string.IsNullOrEmpty(name))
+                query = query.Where(x => x.Name.Contains(name));
+
+            return query.OrderBy(x=>x.ProductID) .ToList();
+        }
+
+        [HttpPost]       
+        public void Insert([FromBody] ProductInsertRequest request)
+        {
+            //Convertir el request => Model (Serializar)
+
+            Product product = new Product
+            {
+                Name = request.Name,
+                Price = request.Price,
+                IsActive = true,
+                CreatedDate = DateTime.Now
+            };
+
+            _context.Products.Add(product);//Un Modelo
+            _context.SaveChanges();
+        }
+        [HttpPut]
+        public void Update([FromBody] Product Product)
+        {
+            _context.Entry(Product).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        [HttpPut]
+        public void UpdatePrice([FromBody] ProductUpdateRequest request)
+        {
+
+            var product = _context.Products.Find(request.Id);
+            product.Price = request.Price;
+            _context.Entry(product).State = EntityState.Modified;         
+           
+            _context.SaveChanges();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var Product = _context.Products.Find(id);
+            Product.IsActive = false;
+            _context.Entry(Product).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+    }
+}
